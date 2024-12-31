439052b2d4be9d449a4939c138f16752a33177a0
Your Name <k.bristi057@gmail.com>
2024-12-31 21:32:12 +0600

    loading data using mongodb

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -16,8 +16,10 @@ const FoodCard = ({ item }) => {
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
 
-          <div className="card-actions justify-center">
-            <button className="btn btn-primary">Add to Cart</button>
+          <div className="card-actions justify-center mt-3">
+            <button className="btn btn-outline border-0 border-b-4 border-[#BB8506] text-[#BB8506]  text-sm px-4 mt-2  ">
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
