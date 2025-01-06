553036f893173fcf482e5c567710edd7b394a4b2
Your Name <k.bristi057@gmail.com>
2025-01-06 19:40:32 +0600

    added the admin route

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../Hooks/useAdmin";
+import useAuth from "../Hooks/useAuth";
+import Loading from "../components/Loading/Loading";
+
+const AdminRoute = (children) => {
+  const [user, loading] = useAuth();
+  const [isAdmin, isAdminLoading] = useAdmin();
+
+  const location = useLocation();
+  if (loading || isAdminLoading) {
+    return <Loading></Loading>;
+  }
+  if (user && isAdmin) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default AdminRoute;
